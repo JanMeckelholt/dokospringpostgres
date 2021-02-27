5f62b4ba2e0e849e5bf32ed8affab7dc30697f40
JanMeckelholt <Jan_Meckelholt@web.de>
2021-02-27 12:58:15 +0100

    Initial commit

---
+++ src/main/java/de/janmeckelholt/dokospringpostgres/DokospringpostgresApplication.java
@@ -0,0 +1,13 @@
+package de.janmeckelholt.dokospringpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokospringpostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DokospringpostgresApplication.class, args);
+    }
+
+}
